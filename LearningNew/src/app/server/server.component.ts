f2e687d8fcf303e14dccb16560d2099b4af0c124
karanc324 <karannsingh58@gmail.com>
2020-01-14 11:46:24 +0530

    1st

---
+++ LearningNew/src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // configuration   metadata for the class
+    // selector should be specific, anyThing
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
