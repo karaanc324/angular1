cf47b8fc45d25550d7086eb6c561a45e8bba8904
Apna Ubuntu <karannsingh58@gmail.com>
2020-04-25 21:45:07 +0530

    seperate components uptill view init (@Input @Output)

---
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+
+  constructor() { }
+
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  newServerName = '';
+  newServerContent = '';
+  ngOnInit(): void {
+  }
+
+  onAddServer() {
+    console.log('addserver', this.newServerName)
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent});
+  }
+
+  onAddBlueprint() {
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    })
+  }
+}
